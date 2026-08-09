35d71177fa27491e86bd3858882a2bd5a1efb13c
hieubin <hn0622068@gmail.com>
2026-08-09 23:11:02 +0700

    .

--- experiments/day8/lib/prisma.js
+++ experiments/day8/lib/prisma.js
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
 
 export default prisma;
